fe96a0b088d075db72699b3e3213876caa4ff8d2
sainath <bhumireddyrajareddy89@gmail.com>
2025-03-22 17:07:14 +0530

    removed special offers

--- restaurantserver/index.js
+++ restaurantserver/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import UserRouter from './login.js';
 import MenuRouter from './menu.js';
-import spoffersRouter from "./specialoffers.js"
 import CartRouter from "./cart.js";
 
 
@@ -15,7 +14,6 @@ app.use(cors());
 
 app.use("/api/user", UserRouter);  
 app.use("/api/menu", MenuRouter);
-app.use("/api/spoffers",spoffersRouter);
 app.use("/api/cart", CartRouter);
 
 
